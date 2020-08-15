ddf5524ac5480395bd023f94e9c8400f05a7dac9
fluturecode <fluturecode@gmail.com>
2020-08-14 22:19:31 -0400

    latest version

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Header from "../";
 function App() {
   return (
     <div className="App">
-      <h1>My Slack</h1>My Slack
+      <h1>My Slack</h1>
       <Header />
       {/*sidebar*/}
       {/*react-router*/}
