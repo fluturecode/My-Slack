ae5114c54b8be364a815a3adf93f7e920a3ef018
fluturecode <fluturecode@gmail.com>
2020-08-15 22:16:36 -0400

    added sidebar options

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from "react";
 import "./App.css";
 import Header from "./components/Header";
+import Sidebar from "./components/Sidebar";
 
 function App() {
   return (
     <div className="App">
       <h1>My Slack</h1>
       <Header />
-      {/*sidebar*/}
+      <div className="app__body">
+        <Sidebar />
+      </div>
       {/*react-router*/}
     </div>
   );
