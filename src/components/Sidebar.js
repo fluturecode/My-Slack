b4f7ca3012dff6c3e7f82f52f9cf2dfe22bd8627
fluturecode <fluturecode@gmail.com>
2020-08-16 15:16:14 -0400

    latest commit

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -4,6 +4,13 @@ import FiberManualRecordIcon from "@material-ui/icons/FiberManualRecord";
 import CreateIcon from "@material-ui/icons/Create";
 import InsertCommentIcon from "@material-ui/icons/InsertComment";
 import SidebarOption from "./SidebarOption";
+import InboxIcon from "@material-ui/icons/Inbox";
+import DraftsIcon from "@material-ui/icons/Drafts";
+import BookmarkBorderIcon from "@material-ui/icons/BookmarkBorder";
+import FileCopyIcon from "@material-ui/icons/FileCopy";
+import PeopleAltIcon from "@material-ui/icons/PeopleAlt";
+import AppsIcon from "@material-ui/icons/Apps";
+import ExpandLessIcon from "@material-ui/icons/ExpandLess";
 
 function Sidebar() {
   return (
@@ -19,7 +26,16 @@ function Sidebar() {
         <CreateIcon />
       </div>
       <SidebarOption Icon={InsertCommentIcon} title="Threads" />
-      <SidebarOption title="YouTube" />
+      <SidebarOption Icon={InboxIcon} title="Mentions & Reactions" />
+      <SidebarOption Icon={DraftsIcon} title="Saved Items" />
+      <SidebarOption Icon={BookmarkBorderIcon} title="Channel Browser" />
+      <SidebarOption Icon={PeopleAltIcon} title="People & User Groups" />
+      <SidebarOption Icon={AppsIcon} title="Apps" />
+      <SidebarOption Icon={FileCopyIcon} title="File Browser" />
+      <SidebarOption Icon={ExpandLessIcon} title="Show Less" />
+      <hr />
+      <SidebarOption Icon={ExpandLessIcon} title="Show Less" />
+      <SidebarOption Icon={ExpandLessIcon} title="Show Less" />
     </div>
   );
 }
